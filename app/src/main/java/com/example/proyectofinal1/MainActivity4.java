9f3febf7081d3ba422f65c42c42a916a40a95693
klismaAlbornoz <albornozcajasklisman@gmail.com>
2021-10-24 12:51:03 -0500

    primera semana

---
+++ app/src/main/java/com/example/proyectofinal1/MainActivity4.java
@@ -0,0 +1,21 @@
+package com.example.proyectofinal1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+    }
+
+    public void aceptarContinuar(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity5.class);
+        startActivity(intent);
+    }
+}
